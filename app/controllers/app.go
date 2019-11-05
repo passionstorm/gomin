7b19e991464a42120d5f45a8050d8d9271110adc
minhvh <minhvh@soft-flight.work>
2019-11-05 15:11:44 +0700

    init gomin

---
+++ app/controllers/app.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"github.com/revel/revel"
+)
+
+type App struct {
+	*revel.Controller
+}
+
+type BaseController struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
