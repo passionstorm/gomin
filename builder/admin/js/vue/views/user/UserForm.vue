e0800d858d5a9b864ea00425e487fc597d17a234
minhvh93 <vohoangminhdn93@gmail.com>
2019-11-11 00:25:12 +0700

    init

---
+++ builder/admin/js/vue/views/user/UserForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form>
+    <v-text title="Username" v-model="form.username" disabled/>
+    <v-text title="Name" v-model="form.name"/>
+    <v-text title="Email" v-model="form.email"/>
+    <v-text title="Phone" v-model="form.phone"/>
+
+  </form>
+</template>
+
+<script>
+  import VText from '../../widgets/VText';
+  export default {
+    components:{
+      VText
+    },
+    props:{
+      data: {
+        type: Object,
+        default: {
+          username: '',
+          email: '',
+          phone: '',
+          name: '',
+        },
+      }
+    },
+    data(){
+      return{
+        f: this.data
+      }
+    },
+    watch: {
+      data: function(newVal, old) { // watch it
+        console.log(newVal);
+        this.f = newVal
+      }
+    },
+    computed:{
+      form: {
+        get: function() {
+          return this.data
+        },
+        set: function(){
+
+        }
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
--- builder/admin/js/vue/views/user/index.vue
+++ builder/admin/js/vue/views/user/index.vue
@@ -1,41 +1,52 @@
 <template>
   <div>
-    <div class="table-responsive">
-      <table class="table table-striped">
-        <thead>
-        <tr>
-          <th v-for="header in headers" scope="col">{{header}}</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="i in users">
-          <th scope="row">{{i.username}}</th>
-          <td>{{i.name}}</td>
-          <td>{{i.email}}</td>
-          <td>{{i.phone}}</td>
-          <td>
-            <div class="d-flex">
-              <icon class="btn btn-icon" name="pen" color="red"></icon>
-              <icon class="btn btn-icon" name="trash"></icon>
-            </div>
+    <div class="row">
+      <div class="col-7">
+        <div class="table-responsive">
+          <table class="table table-striped">
+            <thead>
+            <tr>
+              <th v-for="header in headers" scope="col">{{header}}</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="i in users">
+              <th scope="row">{{i.username}}</th>
+              <td>{{i.name}}</td>
+              <td>{{i.email}}</td>
+              <td>{{i.phone}}</td>
+              <td>
+                <div class="d-flex">
+                  <button type="button" class="btn" @click="edit(i)">
+                    <icon class="btn btn-icon" name="pen" color="red" ></icon>
+                  </button>
 
-          </td>
-        </tr>
-        </tbody>
-      </table>
+                  <icon class="btn btn-icon" name="trash"></icon>
+                </div>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="col">
+        <user-form :data="selected"/>
+      </div>
     </div>
+
   </div>
 </template>
 
 <script>
   import Icon from '../../widgets/Icon';
   import json from './user.json';
-
+  import UserForm from './UserForm';
   export default {
-    components: {Icon},
+    components: {Icon,UserForm},
     data() {
       return {
         users: json,
+        itemSelected: {}
       };
     },
     computed: {
@@ -45,7 +56,20 @@
           'username', 'name', 'email', 'phone', '',
         ];
       },
+      selected(){
+        return this.itemSelected
+      }
     },
+    methods:{
+      edit(item){
+        this.itemSelected = item;
+      }
+    },
+    watch:{
+      itemSelected(val){
+        console.log(val);
+      }
+    }
   };
 </script>
 <style scoped>
